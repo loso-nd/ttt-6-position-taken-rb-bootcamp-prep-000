b059973b85ab44c75c9b4dd53b5f80eee6ad215e
Nahomie ND <nducasse3@gmail.com>
2020-12-10 15:46:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,8 +6,8 @@ def position_taken?(board, index)
   if (board[index] == "X" || board[index] == "O")
     true
     
-  elsif (board[index] == " " || board[index] == "")
-     false
+  #elsif (board[index] == " " || board[index] == "")
+     #false
      
   else board[index] == nil
     false
